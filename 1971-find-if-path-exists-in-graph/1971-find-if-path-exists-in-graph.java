2362414227a68e5fed3b9a36eb8714a46c49717e
Shivam Joshi <joshishivam2004@gmail.com>
2026-07-27 08:20:29 +0530

    Time: 150 ms (34.05%), Space: 296.8 MB (25.6%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,4 +1,10 @@
 class Solution {
+    void dfs(List<List<Integer>>  list,int source,boolean[] check){
+        check[source] = true;
+        for(int i = 0; i < list.get(source).size(); i++){
+            if(check[list.get(source).get(i)] == false ) dfs(list,list.get(source).get(i),check);
+        }
+    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> list = new ArrayList<>();
         for(int i = 0; i < n; i++){
@@ -8,21 +14,9 @@ class Solution {
             list.get(edge[0]).add(edge[1]);
             list.get(edge[1]).add(edge[0]);
         }
-        Queue<Integer> qu = new LinkedList<>();
-        qu.add(source);
         boolean[] check = new boolean[n];
-        check[source] = true;
-        while(!qu.isEmpty()){
-            int top = qu.remove();
-            for(int i = 0; i < list.get(top).size(); i++){
-                if(!check[list.get(top).get(i)]){
-                    qu.add(list.get(top).get(i));
-                    check[list.get(top).get(i)] = true;
-                    if(list.get(top).get(i) == destination) return true;
-                }
-            }
-            
-        }
+        dfs(list,source,check);   
+        
         if(!check[destination]) return false;
         else return true;
     }
