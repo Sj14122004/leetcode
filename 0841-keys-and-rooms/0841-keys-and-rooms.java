fe467a685112aca522a37be3cef2b62f5f5f10bc
Shivam Joshi <joshishivam2004@gmail.com>
2026-07-27 07:26:21 +0530

    Time: 1 ms (83.49%), Space: 46.5 MB (26.54%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,18 +1,38 @@
+// class Solution {
+//     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+//         Queue<Integer> qu = new LinkedList<>();
+//         qu.add(0);
+//         boolean [] check = new boolean[rooms.size()];
+//         check[0] = true;
+//         while(!qu.isEmpty()){
+//             int top = qu.remove();
+//             for(int i = 0; i < rooms.get(top).size(); i++){
+//                 if(!check[rooms.get(top).get(i)]){
+//                     qu.add(rooms.get(top).get(i));;
+//                     check[rooms.get(top).get(i)] = true;
+//                 }
+//             }
+//         }
+//         for(boolean i:check){
+//             if(!i) return false;
+//         }
+//         return true;
+//     }
+// }
 class Solution {
+void dfs(int i, List<List<Integer>> rooms,boolean[] check ){
+    check[i] = true;
+    for(int ele : rooms.get(i)){
+       if(check[ele] == false) dfs(ele,rooms,check);
+    }
+}
+
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        Queue<Integer> qu = new LinkedList<>();
-        qu.add(0);
         boolean [] check = new boolean[rooms.size()];
-        check[0] = true;
-        while(!qu.isEmpty()){
-            int top = qu.remove();
-            for(int i = 0; i < rooms.get(top).size(); i++){
-                if(!check[rooms.get(top).get(i)]){
-                    qu.add(rooms.get(top).get(i));;
-                    check[rooms.get(top).get(i)] = true;
-                }
-            }
-        }
+
+        dfs(0,rooms,check );
+        
+        
         for(boolean i:check){
             if(!i) return false;
         }
